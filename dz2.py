bb2458ce74429a1c1a548fa77771907184bf1113
Berezochka <morozovelesey@gmail.com>
2025-02-01 11:21:01 +0200

    dz2

--- dz2.py
+++ dz2.py
@@ -1,5 +1,7 @@
 age = int(input("Введіть ваш вік: "))
 if age >= 18:
     print("Вхід дозволено!")
-elif age <= 18:
+elif age >=1 <= 18:
     print("Вхід заборонено!")
+elif age <= 0:
+    print("Ви ще не народились!")
